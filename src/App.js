dc579554e18ea29100418a8a133b74dce63a1300
Yarnacle <109094587+Yarnacle@users.noreply.github.com>
2023-04-21 22:46:10 -0500

    created app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const regex = /(?=.)/gm;
+
+	const [text,setText] = useState('');
+	const [numToAdd,setNumToAdd] = useState(0);
+	const [spoofedText,setSpoofedText] = useState('');
+
+	const spoof = e => {
+		e.preventDefault();
+		
+		setSpoofedText(`${text}${' ⠀'.repeat(numToAdd)}`);
+	};
+
+	return (
+		<div className="App">
+			<form onSubmit={spoof}>
+				<textarea placeholder="Text to be obfuscated..." id="text-field" onChange={(e) => setText(e.target.value)} value={text} />
+				<input type="number" onChange={(e) => setNumToAdd(e.target.value)} value={numToAdd} />
+				<input type="submit" />
+			</form>
+			<textarea placeholder="result" readonly value={spoofedText} />
+		</div>
+	);
 }
 
 export default App;
